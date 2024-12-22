d629a80d782ec1e30403d35c452078a367602cfe
pntbr <pntbr@pm.me>
2024-12-22 18:21:52 +0100

    deploie 11ty sur gh-pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,14 @@
+module.exports = function(eleventyConfig) {
+    return {
+      dir: {
+        input: "src",
+        output: "dist"
+      }
+    };
+  };
+  
 export default function (eleventyConfig) {
     eleventyConfig.addCollection("products", function(collectionApi) {
         return collectionApi.getFilteredByGlob("products/*.md");
     });
-}
+};
